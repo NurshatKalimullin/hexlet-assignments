58a9b7f93b42ac94ea810a83f97033c7e59426a1
@hexlet/cli <support@hexlet.io>
2022-10-02 21:48:16 +0300

    submit java-oop-ru/reflections

---
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -0,0 +1,53 @@
+package exercise;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+// BEGIN
+public class Validator {
+
+    public static List<String> validate(Address address) {
+        List<String> errorFields = new ArrayList<>();
+        Field[] fields = address.getClass().getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                NotNull notNull = field.getAnnotation(NotNull.class);
+                field.setAccessible(true);
+                if (notNull != null && Objects.equals((String) field.get(address), null)){
+                    errorFields.add(field.getName());
+                }
+            }
+        } catch (IllegalAccessException e) {
+            System.out.println(e);
+        }
+        return errorFields;
+    }
+
+
+    public static Map<String, List<String>> advancedValidate(Address address) {
+        Field[] fields = address.getClass().getDeclaredFields();
+        Map<String, List<String>> errors = new HashMap<>();
+        try {
+            for (Field field : fields) {
+                NotNull notNull = field.getAnnotation(NotNull.class);
+                MinLength minLength = field.getAnnotation(MinLength.class);
+                field.setAccessible(true);
+                if (notNull != null && Objects.equals((String) field.get(address), null)){
+                    errors.put(field.getName(), List.of("can not be null"));
+                }
+                if (minLength != null && !Objects.equals((String) field.get(address), null)){
+                    int minValue = minLength.minLength();
+                    String value = (String) field.get(address);
+                    if (value.length() < minValue) {
+                        errors.put(field.getName(), List.of("length less than 4"));
+                    }
+                }
+
+            }
+        } catch (IllegalAccessException e) {
+            System.out.println(e);
+        }
+        return errors;
+    }
+}
+// END
